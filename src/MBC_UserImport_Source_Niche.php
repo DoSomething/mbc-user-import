aeecad4c817169721f605b08aad0ce466608cae7
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-01-30 18:48:20 -0500

    Stub all existing Niche logic into new structure

--- src/MBC_UserImport_Source_Niche.php
+++ src/MBC_UserImport_Source_Niche.php
@@ -83,15 +83,95 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
     if (isset($message['email'])) {
       $this->importUser->email = $message['email'];
     }
-    
+
     if (isset($message['name']) && !isset($message['first_name'])) {
       $nameBits = $this->mbcUserImportToolbox->nameBits($message['name']);
       $this->importUser->first_name = $nameBits['first_name'];
       $this->importUser->last_name = $nameBits['last_name'];
     }
-    
+
     $firstName = isset($message['first_name']) && $message['first_name'] != '' ? $message['first_name'] : 'DS';
     $this->importUser->password = str_replace(' ', '', $firstName) . '-Doer' . rand(1, 1000);
+
+
+
+
+
+
+     if (isset($user->birthdate) && $user->birthdate != 0) {
+        $userDetails['birthdate_timestamp'] = strtotime($user->birthdate);
+      }
+      if (isset($user->first_name) && $user->first_name != '') {
+        $userDetails['merge_vars']['FNAME'] = $user->first_name;
+      }
+      if (isset($user->last_name) && $user->last_name != '') {
+        $userDetails['merge_vars']['LNAME'] = $user->last_name;
+      }
+      if (isset($user->password) && $user->password != '') {
+        $userDetails['merge_vars']['PASSWORD'] = $user->password;
+      }
+      if (isset($user->address1) && $user->address1 != '') {
+        $userDetails['address1'] = $user->address1;
+      }
+      if (isset($user->address2) && $user->address2 != '') {
+        $userDetails['address2'] = $user->address2;
+      }
+      if (isset($user->city) && $user->city != '') {
+        $userDetails['city'] = $user->city;
+      }
+      if (isset($user->state) && $user->state != '') {
+        $userDetails['state'] = $user->state;
+      }
+      if (isset($user->zip) && $user->zip != '') {
+        $userDetails['zip'] = $user->zip;
+      }
+      if (isset($user->phone) && $user->phone != '') {
+        $userDetails['mobile'] = $user->phone;
+      }
+      if (isset($user->hs_gradyear) && $user->hs_gradyear != 0) {
+        $userDetails['hs_gradyear'] = $user->hs_gradyear;
+      }
+      if (isset($user->race) && $user->race != '') {
+        $userDetails['race'] = $user->race;
+      }
+      if (isset($user->religion) && $user->religion != '') {
+        $userDetails['religion'] = $user->religion;
+      }
+      if (isset($user->hs_name) && $user->hs_name != '') {
+        $userDetails['hs_name'] = $user->hs_name;
+      }
+      if (isset($user->college_name) && $user->college_name != '') {
+        $userDetails['college_name'] = $user->college_name;
+      }
+      if (isset($user->major_name) && $user->major_name != '') {
+        $userDetails['major_name'] = $user->major_name;
+      }
+      if (isset($user->degree_type) && $user->degree_type != '') {
+        $userDetails['degree_type'] = $user->degree_type;
+      }
+      if (isset($user->sat_math) && $user->sat_math != 0) {
+        $userDetails['sat_math'] = $user->sat_math;
+      }
+      if (isset($user->sat_verbal) && $user->sat_verbal != 0) {
+        $userDetails['sat_verbal'] = $user->sat_verbal;
+      }
+      if (isset($user->sat_writing) && $user->sat_writing != 0) {
+        $userDetails['sat_writing'] = $user->sat_writing;
+      }
+      if (isset($user->act_math) && $user->act_math != 0) {
+        $userDetails['act_math'] = $user->act_math;
+      }
+      if (isset($user->gpa) && $user->gpa != 0) {
+        $userDetails['gpa'] = $user->gpa;
+      }
+      if (isset($user->role) && $user->role != '') {
+        $userDetails['role'] = $user->role;
+      }
+
+
+
+
+
 
   }
 
@@ -102,31 +182,35 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
    */
   public function process() {
     
-    // Check for existing account
-    $existing = $this->mbcUserImportToolbox->checkExisting();
+    $existing = NULL;
+    $payload = NULL;
     
-    // create Drupal user
+    // Add welcome email details to payload
+    $this->mbcUserImportToolbox->addWelcomeEmail($this->importUser, $payload);
+    
+    // Check for existing email account in MailChimp
+    $existing['email'] = $this->mbcUserImportToolbox->checkExisting($this->importUser, 'email');
+    if (empty($existing['email'])) {
+      $this->mbcUserImportToolbox->addEmailSubscription($this->importUser, $payload);
+    }
+    
+    // Drupal user
+    $existing['drupal'] = $this->mbcUserImportToolbox->checkExisting($this->importUser, 'drupal');
     if (empty($existing['drupal'])) {
       $this->drupalUID = $this->mbcUserImportToolbox->createDrupalUser($this->importUser);
-    }
-    else {
-      $this->drupalUID = $existing['drupal'];
+      $this->mbcUserImportToolbox->sendPasswordResetEmail($this->importUser);
     }
 
-    // send welcome email
-    $this->mbcUserImportToolbox->sendWelcomeEmail();
+    // Check for existing user account in Mobile Commons
+    $existing['sms'] = $this->mbcUserImportToolbox->checkExisting($this->importUser, 'sms');
     
-    // send welcome SMS
-    if (empty($existing['mobile'])) {
-      $this->mbcUserImportToolbox->sendWelcomeSMS();
-    }
-    else {
-      
-    }
+    // Add SMS welcome details to payload
+    $this->mbcUserImportToolbox->addWelcomeSMS($this->importUser, $payload);
     
-    // send password reset email
-    $this->mbcUserImportToolbox->sendPasswordResetEmail();
-
+    // @todo: transition to using JSON formatted messages when all of the consumers are able to
+    // detect the message format and process either seralized or JSON.
+    $message = seralize($payload);
+    $this->messageBroker->publishMessage($message, 'user.registration.transactional');
   }
 
 }
