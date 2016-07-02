5c8097e101fbcc244ab39f781b70822cf9c33f47
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-07-02 03:57:55 -0400

    Passes Lint test

--- src/MBC_UserImport_Source_Niche.php
+++ src/MBC_UserImport_Source_Niche.php
@@ -1,6 +1,19 @@
 <?php
 /**
+ * A consumer application for a user import system. Various source types define
+ * specifics as to how the user data should be injested from CSV files. User data is
+ * distributed to other consumers in the Quicksilver system for further processing
+ * as well as triggering transactional messaging related to welcoming the user to
+ * DoSomething.org.
  *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  MBC_UserImport
+ * @author   DeeZone <dlee@dosomething.org>
+ * @license  MIT: https://en.wikipedia.org/wiki/MIT_License
+ * @version  GIT: <git_id>
+ * @link     https://github.com/DoSomething/mbc-user-import
  */
  
 namespace DoSomething\MBC_UserImport;
@@ -8,19 +21,31 @@ namespace DoSomething\MBC_UserImport;
 use DoSomething\MBC_UserImport\MBC_UserImport_BaseSource;
 use \Exception;
 
+/**
+ * Class MBC_UserImport_Source_Niche: Functionality specific to processing users
+ * the source niche.com.
+ *
+ * @category PHP
+ * @package  MBC_UserImport
+ * @author   DeeZone <dlee@dosomething.org>
+ * @license  MIT: https://en.wikipedia.org/wiki/MIT_License
+ * @version  "Release: <package_version>"
+ * @link     https://github.com/DoSomething/mbc-user-import
+ */
 class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
 {
 
     const WELCOME_EMAIL_NEW_NEW = 'mb-niche-welcome_new-new_v1-1-1';
     const WELCOME_EMAIL_EXISTING_NEW = 'mb-niche-welcome_existing-new_v1-1-0';
-    const WELCOME_EMAIL_EXISTING_EXISTING = 'mb-niche-welcome_existing-existing_v1-1-0';
+    const WELCOME_EMAIL_EXISTING_EXISTING
+        = 'mb-niche-welcome_existing-existing_v1-1-0';
     const MOBILE_COMMONS_SIGNUP = 207601; // Shower Song
     const PHOENIX_SIGNUP = 3590; // Shower Song
 
     /**
-   * Constructor for MBC_UserImport_Source_Nice - extension of the base source class
-   * that's specific to Niche.
-   */
+     * Constructor for MBC_UserImport_Source_Nice - extension of the base source
+     * class that's specific to Niche.
+     */
     public function __construct()
     {
 
@@ -30,12 +55,14 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
     }
 
     /**
-   * canProcess(): Test if message can be processed by consumer. Niche user imports must have at
-   * least an email address.
-   *
-   * @param array
-   *   The message contents to test if it can be processed.
-   */
+     * Test if message can be processed by consumer.
+     *
+     * @param array $message The message contents to test if it can be processed.
+     *
+     * @return boolean Can the message be processed.
+     *
+     * @throws Exception
+     */
     public function canProcess($message)
     {
     
@@ -46,12 +73,18 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
         }
 
         if (filter_var($message['email'], FILTER_VALIDATE_EMAIL) === false) {
-            echo '- canProcess(), failed FILTER_VALIDATE_EMAIL: ' . $message['email'], PHP_EOL;
+            echo '- canProcess(), failed FILTER_VALIDATE_EMAIL: '
+              . $message['email'], PHP_EOL;
             parent::reportErrorPayload();
-            throw new Exception('canProcess(), failed FILTER_VALIDATE_EMAIL: ' . $message['email']);
-        }
-        elseif (isset($message['email'])) {
-            $this->message['email'] = filter_var($message['email'], FILTER_VALIDATE_EMAIL);
+            throw new Exception(
+                'canProcess(), failed FILTER_VALIDATE_EMAIL: '
+                . $message['email']
+            );
+        } elseif (isset($message['email'])) {
+            $this->message['email'] = filter_var(
+                $message['email'],
+                FILTER_VALIDATE_EMAIL
+            );
         }
 
         if (isset($message['email']) && empty($message['mailchimp_list_id'])) {
@@ -62,18 +95,18 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
     }
 
     /**
-   * setter(): Assign values from message to class propertry for processing.
-   *
-   * @param array
-   *   The values from the message consumed from the queue.
-   */
+     * Assign values from message to class propertry for processing.
+     *
+     * @param array $message The values from the message consumed from the queue.
+     *
+     * @return null
+     */
     public function setter($message)
     {
 
         if (isset($message['source'])) {
             $this->importUser['user_registration_source'] = $message['source'];
-        }
-        else {
+        } else {
             $this->importUser['user_registration_source'] = 'Niche';
         }
         if (isset($message['source_file'])) {
@@ -96,22 +129,28 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
             $this->importUser['last_name'] = $nameBits['last_name'];
         }
 
-        $firstName = isset($message['first_name']) && $message['first_name'] != '' ? $message['first_name'] : 'DS';
-        $this->importUser['password'] = str_replace(' ', '', $firstName) . '-Doer' . rand(1, 1000);
+        $firstName = isset($message['first_name']) && $message['first_name'] != ''
+          ? $message['first_name'] : 'DS';
+        $this->importUser['password']
+            = str_replace(' ', '', $firstName) . '-Doer' . rand(1, 1000);
 
         // Optional fields
         if ($message['birthdate'] > time()) {
-            echo '- WARNING: Invalid birthdate: ' . $message['birthdate'] . ' -> date(): ' . date('r', $message['birthdate']), PHP_EOL;
-        }
-        else {
+            echo '- WARNING: Invalid birthdate: ' . $message['birthdate'] .
+              ' -> date(): ' . date('r', $message['birthdate']), PHP_EOL;
+        } else {
             if (isset($message['birthdate']) && is_int($message['birthdate'])) {
                 $this->importUser['birthdate_timestamp'] = $message['birthdate'];
-            }
-            elseif (isset($message['birthdate']) && ctype_digit($message['birthdate'])) {
-                $this->importUser['birthdate_timestamp'] = (int) $message['birthdate'];
-            }
-            elseif (isset($message['birthdate']) && is_string($message['birthdate'])) {
-                $this->importUser['birthdate_timestamp'] = strtotime($message['birthdate']);
+            } elseif (isset($message['birthdate'])
+                && ctype_digit($message['birthdate'])
+            ) {
+                $this->importUser['birthdate_timestamp']
+                    = (int) $message['birthdate'];
+            } elseif (isset($message['birthdate'])
+                && is_string($message['birthdate'])
+            ) {
+                $this->importUser['birthdate_timestamp']
+                    = strtotime($message['birthdate']);
             }
         }
         if (!empty($message['first_name'])) {
@@ -143,21 +182,20 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
         }
         if (!empty($message['country'])) {
             $this->importUser['country'] = $message['country'];
-        }
-        else {
+        } else {
             $this->importUser['country'] = 'US';
         }
         if (!empty($message['zip'])) {
             $this->importUser['zip'] = $message['zip'];
             $this->importUser['postal_code'] = $message['zip'];
-        }
-        elseif (!empty($message['postal_code'])) {
+        } elseif (!empty($message['postal_code'])) {
             $this->importUser['zip'] = $message['postal_code'];
             $this->importUser['postal_code'] = $message['postal_code'];
         }
         // Validate phone number based on the North American Numbering Plan
         // https://en.wikipedia.org/wiki/North_American_Numbering_Plan
-        $regex = "/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i";
+        $regex
+            = "/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i";
         if ((preg_match($regex, $message['phone']))) {
             $this->importUser['mobile'] = $message['phone'];
         }
@@ -204,9 +242,11 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
     }
 
     /**
-   * process(): Functional hum of class specific to the source. Defined steps specific
-   * to Niche user import.
-   */
+     * Functional hum of class specific to the source. Defined steps specific to
+     * Niche user import.
+     *
+     * @return null
+     */
     public function process()
     {
 
@@ -218,15 +258,20 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
         $this->addWelcomeEmailSettings($this->importUser, $payload);
     
         // Check for existing email account in MailChimp
-        $this->mbcUserImportToolbox->checkExistingEmail($this->importUser, $existing);
+        $this->mbcUserImportToolbox->checkExistingEmail(
+            $this->importUser, $existing
+        );
         if (empty($existing['email'])) {
             $this->addEmailSubscriptionSettings($this->importUser, $payload);
         }
     
         // Drupal user
-        $this->mbcUserImportToolbox->checkExistingDrupal($this->importUser, $existing);
+        $this->mbcUserImportToolbox->checkExistingDrupal(
+            $this->importUser, $existing
+        );
         if (empty($existing['drupal-uid'])) {
-            $northstarUser = $this->mbToolbox->createNorthstarUser((object) $this->importUser);
+            $northstarUser
+                = $this->mbToolbox->createNorthstarUser((object) $this->importUser);
 
             $this->addImportUserInfo($northstarUser->data);
             $drupalUID = $northstarUser->data->drupal_id;
@@ -236,8 +281,7 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
             $payload['tags'][0] = 'user-welcome-niche';
             $payload['tags'][1] = self::WELCOME_EMAIL_NEW_NEW;
             $payload['merge_vars']['PASSWORD_RESET_LINK'] = $passwordResetURL;
-        }
-        else {
+        } else {
             // Existing Drupal user. Set UID for campaign signup
             $drupalUID = $existing['drupal-uid'];
             // #2, current_user, Existing/New
@@ -248,7 +292,9 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
 
         // Campaign signup
         $campaignNID = self::PHOENIX_SIGNUP;
-        $campaignSignup = $this->mbcUserImportToolbox->campaignSignup($campaignNID, $drupalUID, 'niche', false);
+        $campaignSignup = $this->mbcUserImportToolbox->campaignSignup(
+            $campaignNID, $drupalUID, 'niche', false
+        );
         if (!$campaignSignup) {
             // User was not signed up to campaign because they're already signed up.
             // #3, current_signedup, Existing/Existing
@@ -260,50 +306,54 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
         // Check for existing user account in Mobile Commons
         $this->mbcUserImportToolbox->checkExistingSMS($this->importUser, $existing);
     
-        // Add SMS welcome details to payload - all users should be attempted to be added to
-        // MOBILE_COMMONS_SIGNUP opt_id
+        // Add SMS welcome details to payload - all users should be attempted to be
+        // added to MOBILE_COMMONS_SIGNUP opt_id
         $this->addWelcomeSMSSettings($this->importUser, $payload);
 
-        // @todo: transition to using JSON formatted messages when all of the consumers are able to
+        // @todo: transition to using JSON formatted messages when all of the
+        // consumers are able to
         // detect the message format and process either seralized or JSON.
         $message = serialize($payload);
-        $this->messageBroker_transactionals->publish($message, 'user.registration.transactional');
-        $this->statHat->ezCount('mbc-user-import: MBC_UserImport_Source_Niche: process', 1);
+        $this->messageBroker_transactionals->publish(
+            $message, 'user.registration.transactional'
+        );
+        $this->statHat->ezCount(
+            'mbc-user-import: MBC_UserImport_Source_Niche: process', 1
+        );
 
         // Log existing users
         $this->mbcUserImportToolbox->logExisting($existing, $this->importUser);
     }
 
     /**
-   * addWelcomeEmailSettings(): Initial settings related to initial welcome messages.
-   *
-   * @param array $user
-   *   User settings
-   * @param array $payload
-   *   Settings for submission to service specific to the user and what they signed up for.
-   */
+     * Initial settings related to initial welcome messages.
+     *
+     * @param array $user    User settings.
+     * @param array $payload Service values specific to the user.
+     *
+     * @return array $payload Service values specific to the user.
+     */
     public function addWelcomeEmailSettings($user, &$payload)
     {
 
         $payload['email'] = $user['email'];
         $payload['merge_vars'] = [
-        'MEMBER_COUNT' => $this->memberCount,
-        'FNAME' => $user['first_name']
+          'MEMBER_COUNT' => $this->memberCount,
+          'FNAME' => $user['first_name']
         ];
         $payload['tags'] = [
-        0 => 'user_welcome-niche',
+          0 => 'user_welcome-niche',
         ];
     }
 
     /**
-   * addCommonPayload(): OPayload values common to all message for submission to all services and exchanges.
-   *
-   * @param array $user
-   *   Values related to the user being processed.
-   *
-   * @return array $payload
-   *   Constructed values for submission to exchanges for message distribution.
-   */
+     * Payload values common to all message for submission to all services and
+     * exchanges.
+     *
+     * @param array $user Values related to the user being processed.
+     *
+     * @return array $payload Values for message distribution.
+     */
     public function addCommonPayload($user)
     {
 
@@ -315,32 +365,31 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
     }
 
     /**
-   * addEmailSubscriptionSettings(): Settings related to email services.
-   *
-   * @param array $user
-   *   User settings
-   * @param array $payload
-   *   Settings for submission to service specific to the user and what they signed up for.
-   */
+     * Settings related to email services.
+     *
+     * @param array $user    User settings
+     * @param array $payload Settings for submission to service.
+     *
+     * @return array $payload Settings for submission to service.
+     */
     public function addEmailSubscriptionSettings($user, &$payload)
     {
 
         if (isset($user['mailchimp_list_id'])) {
             $payload['mailchimp_list_id'] = $user['mailchimp_list_id'];
-        }
-        else {
+        } else {
             $payload['mailchimp_list_id'] = 'f2fab1dfd4';
         }
     }
 
     /**
-   * addWelcomeSMSSettings(): Settings related to SMS services.
-   *
-   * @param array $user
-   *   User settings
-   * @param array $payload
-   *   Settings for submission to service specific to the user and what they signed up for.
-   */
+     * Settings related to SMS services.
+     *
+     * @param array $user    User settings
+     * @param array $payload Settings for submission to service.
+     *
+     * @return array $payload Settings for submission to service.
+     */
     public function addWelcomeSMSSettings($user, &$payload)
     {
 
@@ -351,19 +400,22 @@ class MBC_UserImport_Source_Niche extends MBC_UserImport_BaseSource
     }
 
     /**
-   * sendPasswordResetEmail(): Details about sending password reset email.
-   */
+     * Details about sending password reset email.
+     *
+     * @return null
+     */
     public function sendPasswordResetEmail()
     {
 
     }
 
     /**
-   * addImportUserInfo() Details about the Drulal user created for the user import.
-   *
-   * @param object $drupalUser
-   *   The user object created by the call to the Drupal API.
-   */
+     * Details about the Drulal user created for the user import.
+     *
+     * @param object $drupalUser The user object created by Drupal API.
+     *
+     * @return null
+     */
     public function addImportUserInfo($drupalUser)
     {
 
