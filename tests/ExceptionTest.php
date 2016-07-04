0af59f1896c77d13f6e6be78752a5ec2b246f3ff
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-07-04 17:01:35 -0400

    Add Exception tests

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Test coverage for Exceptions in MBC_UserImport application.
+ */
+
+namespace DoSomething\MBC_UserImport;
+
+/**
+ * Class ExceptionTest
+ *
+ * @package DoSomething\MBC_UserImport
+ */
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var object MBC_UserImport_Consumer.
+     */
+    public $mbcUserImportConsumer;
+
+    /**
+     * @var array allowedSources.
+     */
+    public $allowedSources;
+
+    /**
+     * Common functionality to all tests. Load configuration settings and properties.
+     */
+    public function setUp()
+    {
+        require_once __DIR__ . '/../mbc-user-import.config.inc';
+        $this->mbcUserImportConsumer = new MBC_UserImport_Consumer();
+        $this->allowedSources = unserialize(ALLOWED_SOURCES);
+    }
+
+    /**
+     * @covers \DoSomething\MBC_UserImport\MBC_UserImport_Consumer::canProcess
+     * @uses   \DoSomething\MBC_UserImport\MBC_UserImport_Consumer
+     * @expectedException Exception
+     */
+    public function testException()
+    {
+        // Empty source
+        $message = [];
+        $this->mbcUserImportConsumer->canProcess($message);
+
+        // Unsupported source
+        $message['source'] = 'TeenLife';
+        $this->mbcUserImportConsumer->canProcess($message);
+    }
+}
